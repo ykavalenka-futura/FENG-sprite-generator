c66f4537e6c9b0c19e733dd4c7425a97a7d3d641
y.kavalenka <y.kavalenka@futura-solutions.de>
2024-04-24 16:30:53 +0200

    Update sprite generator to new version

--- FEngSpriteGenerator/Program.cs
+++ FEngSpriteGenerator/Program.cs
@@ -1,4 +1,6 @@
-﻿using SixLabors.ImageSharp;
+﻿using System.ComponentModel;
+using System.Text;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
@@ -8,15 +10,31 @@ namespace FEngSpriteGenerator
     {
         private static void CustomConsoleMessage(string message, ConsoleColor customColor = ConsoleColor.Red)
         {
-            ConsoleColor originalColor = Console.ForegroundColor;
             Console.ForegroundColor = customColor;
             Console.Error.WriteLine(message);
-            Console.ForegroundColor = originalColor;
+            Console.ResetColor();
+        }
+
+        enum ArgumentKey
+        {
+            InputDir,
+            OutputDir,
+            Align,
+            OutputFileName,
+            PaddingValue,
+            SpriteCssClassName,
+            SpriteCssItemFormat,
+            OutputCssFileName,
+            OutputCssDir
         }
 
         static void Main(string[] args)
         {
+            
             const string defaultOutputFileName = "sprite_result.png";
+            const string defaultOutputCssFileName = "sprite_result.css";
+            const string defaultCssSpriteClass = "sprite";
+            const string defaultCssSpriteNameFormat = ".sprite-icon-{0}";
             const int defaultPadding = 10;
             const AlignSprite defaultAlign = AlignSprite.Vertical;
             
@@ -24,13 +42,19 @@ namespace FEngSpriteGenerator
             CustomConsoleMessage("|\t\u00a9 Futura Solutions GmbH.\t|", ConsoleColor.Blue);
             CustomConsoleMessage("-----------------------------------------", ConsoleColor.Blue);
             
-            Console.WriteLine("Usage: FEngSpriteGenerator <input_directory> <output_directory> "
-                  + "[align] [output_filename] [padding_value]");
-            Console.WriteLine("\t<input_directory>  \t- Path to the directory containing input images.");
-            Console.WriteLine("\t<output_directory> \t- Path to the directory where the generated sprite will be saved.");
-            Console.WriteLine($"\t-align            \t- Optional. Alignment of the generated sprite (V for vertical, H for horizontal). Defaults to '{defaultAlign}'.");
-            Console.WriteLine($"\t-output_filename  \t- Optional. Name of the generated sprite file. Defaults to '{defaultOutputFileName}'.'");
-            Console.WriteLine($"\t-padding_value    \t- Optional. Padding between images in the sprite. Defaults to '{defaultPadding}' pixels.");
+            Console.WriteLine($"Usage: FEngSpriteGenerator --{ArgumentKey.InputDir}=<value> --{ArgumentKey.OutputDir}=<value> "
+                  + $"[--{ArgumentKey.Align}=<value>] [--{ArgumentKey.OutputFileName}=<value>] [--{ArgumentKey.PaddingValue}=<value>] "
+                  + $"[--{ArgumentKey.SpriteCssClassName}=<value>] [--{ArgumentKey.SpriteCssItemFormat}=<value>] "
+                  + $"[--{ArgumentKey.OutputCssFileName}=<value>] [--{ArgumentKey.OutputCssDir}=<value>]");
+            Console.WriteLine($"\t--{ArgumentKey.InputDir}              \t- Path to the directory containing input images.");
+            Console.WriteLine($"\t--{ArgumentKey.OutputDir}             \t- Path to the directory where the generated sprite will be saved.");
+            Console.WriteLine($"\t--{ArgumentKey.Align}                 \t- Optional. Alignment of the generated sprite (V for vertical, H for horizontal). Defaults to '{defaultAlign}'.");
+            Console.WriteLine($"\t--{ArgumentKey.OutputFileName}        \t- Optional. Name of the generated sprite file. Defaults to '{defaultOutputFileName}'.'");
+            Console.WriteLine($"\t--{ArgumentKey.PaddingValue}          \t- Optional. Padding between images in the sprite. Defaults to '{defaultPadding}' pixels.");
+            Console.WriteLine($"\t--{ArgumentKey.SpriteCssClassName}    \t- Optional. Name of base sprite css class. Defaults to '{defaultCssSpriteClass}'.");
+            Console.WriteLine($"\t--{ArgumentKey.SpriteCssItemFormat}   \t- Optional. Name of generated sprite class format. Defaults to '{defaultCssSpriteNameFormat}'.");
+            Console.WriteLine($"\t--{ArgumentKey.OutputCssFileName}     \t- Optional. Exported css file name. Defaults to '{defaultOutputCssFileName}'.");
+            Console.WriteLine($"\t--{ArgumentKey.OutputCssDir}          \t- Optional. Path to the directory to export generated css. Defaults to output directory.");
             
             // Check that all required arguments were set.
             if (args.Length < 2)
@@ -39,15 +63,85 @@ namespace FEngSpriteGenerator
                 return;
             }
 
-            string inputDirectory = args[0];
-            string outputDirectory = args[1];
+            string inputDirectory = string.Empty;
+            string outputDirectory = string.Empty;
+            AlignSprite align = defaultAlign;
+            string outputFileName = defaultOutputFileName;
+            int padding = defaultPadding;
+            string cssClass = defaultCssSpriteClass;
+            string cssSpriteNameFormat = defaultCssSpriteNameFormat;
+            string outputCssFileName = defaultOutputCssFileName;
+            string outputCssDirectory = string.Empty;
+
+            foreach (var arg in args)
+            {
+                if (string.IsNullOrEmpty(arg))
+                    break;
+                string[] keyValuePair = arg.Split("=");
+
+                if (keyValuePair?.Length < 2 || string.IsNullOrEmpty(keyValuePair?[0]) || string.IsNullOrEmpty(keyValuePair[1]))
+                    break;
+
+                string  argumentKey = keyValuePair[0].TrimStart('-'), 
+                        argumentValue = keyValuePair[1].TrimStart('\"').TrimEnd('\"');
+
+                if (!Enum.TryParse(argumentKey, out ArgumentKey argumentKeyEnum))
+                    break;
+
+                switch (argumentKeyEnum)
+                {
+                    case ArgumentKey.InputDir:
+                    {
+                        if (!HasArgument(argumentValue, string.Empty, out inputDirectory) || string.IsNullOrEmpty(inputDirectory) || !Directory.Exists(inputDirectory))
+                            CustomConsoleMessage($"Input '{inputDirectory}' directory does not exist.");
+                        break;
+                    }
+                    case ArgumentKey.OutputDir:
+                    {
+                        if (!HasArgument(argumentValue, string.Empty, out outputDirectory) || string.IsNullOrEmpty(outputDirectory))
+                            CustomConsoleMessage($"Parameter of output directory does not specified. Value: '{outputDirectory}'.");
+                        break;
+                    }
+                    case ArgumentKey.Align: 
+                    {
+                        HasArgument(argumentValue, defaultAlign, out align);
+                        break; 
+                    }
+                    case ArgumentKey.OutputFileName:
+                    {
+                        HasArgument(argumentValue, defaultOutputFileName, out outputFileName);
+                        break;
+                    }
+                    case ArgumentKey.PaddingValue:
+                    {
+                        HasArgument(argumentValue, defaultPadding, out padding);
+                        break;
+                    }
+                    case ArgumentKey.SpriteCssClassName:
+                    {
+                        HasArgument(argumentValue, defaultCssSpriteClass, out cssClass);
+                        break;
+                    }
+                    case ArgumentKey.SpriteCssItemFormat:
+                    {
+                        HasArgument(argumentValue, defaultCssSpriteNameFormat, out cssSpriteNameFormat);
+                        break;
+                    }
+                    case ArgumentKey.OutputCssFileName:
+                    {
+                        HasArgument(argumentValue, defaultOutputCssFileName, out outputCssFileName);
+                        break;
+                    }
+                    case ArgumentKey.OutputCssDir:
+                    {
+                        HasArgument(argumentValue, outputDirectory, out outputCssDirectory);
+                        break;
+                    }
+                }
+            }
 
             // Check exist and available input directory.
-            if (string.IsNullOrEmpty(inputDirectory) || !Directory.Exists(inputDirectory))
-            {
-                CustomConsoleMessage($"Input '{inputDirectory}' directory does not exist.");
-                return;
-            }
+            
             if (string.IsNullOrEmpty(outputDirectory))
             {
                 CustomConsoleMessage($"Parameter of output directory does not specified. Value: '{outputDirectory}'.");
@@ -67,31 +161,16 @@ namespace FEngSpriteGenerator
                     return;
                 }
             }
-
-            AlignSprite align;
-
-            if (args.Length >= 3 && !string.IsNullOrEmpty(args[2]) && Enum.TryParse(args[2], out align)) { }
-            else
-                align = defaultAlign;
             
-            Console.WriteLine($"Sprite align used: '{align}'");
-
-            string outputFileName = string.Empty;
-            int padding = defaultPadding;
-
-            if (args.Length >= 4 && !string.IsNullOrEmpty(args[3]) && args[3].EndsWith(".png"))
-                outputFileName = args[3];
-            else
-                outputFileName = defaultOutputFileName;
-
-            Console.WriteLine($"Output file generated by: '{defaultOutputFileName}'");
-
-            if (args.Length >= 5 && !string.IsNullOrEmpty(args[4]) && int.TryParse(args[4], out padding))
-            { }
-            else
-                padding = defaultPadding;
-
-            Console.WriteLine($"Padding between images: '{padding}'px");
+            CustomConsoleMessage($"Input directory is: '{inputDirectory}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Output directory is: '{outputDirectory}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Sprite align used: '{align}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Output file generated by: '{defaultOutputFileName}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Padding between images: '{padding}'px", Console.ForegroundColor);
+            CustomConsoleMessage($"Css class is: '.{cssClass}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Css class sprite name format is: '{cssSpriteNameFormat}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Output css file name: '{outputCssFileName}'", Console.ForegroundColor);
+            CustomConsoleMessage($"Output css directory: '{outputCssDirectory}'", Console.ForegroundColor);
 
             // Get list of files in input folder
             string[] imageFiles = Array.Empty<string>();
@@ -137,8 +216,16 @@ namespace FEngSpriteGenerator
             {
                 int x = 0, y = 0;
 
-                foreach (var image in images)
+                StringBuilder cssDocument = new StringBuilder();
+                CreateCssDocument(cssDocument, cssClass, outputCssFileName);
+
+                for (int i = 0; i < images.Length; i++)
                 {
+                    var image = images[i];
+                    var imageFile = Path.GetFileName(imageFiles[i]);
+
+                    AppendCss(cssDocument, imageFile, cssSpriteNameFormat, x, y);
+                    
                     // Append image to the sprite.
                     sprite.Mutate(ctx => ctx.DrawImage(image, new SixLabors.ImageSharp.Point(x, y), 1));
 
@@ -161,7 +248,74 @@ namespace FEngSpriteGenerator
                     CustomConsoleMessage($"Sprite can NOT be saved to {outputPath}");
                     CustomConsoleMessage(e?.Message ?? e?.ToString() ?? string.Empty);
                 }
+                
+                // Save css.
+                string outputCssPath = Path.Combine(outputCssDirectory, outputCssFileName);
+                try
+                {
+                    if (File.Exists(outputCssPath))
+                        File.Delete(outputCssPath);
+                    File.WriteAllText(outputCssPath, cssDocument.ToString());
+                    CustomConsoleMessage($"Css file {outputCssFileName} was generated to '{outputCssPath}'", ConsoleColor.Green);
+                }
+                catch (Exception e)
+                {
+                    CustomConsoleMessage($"Css can NOT be generated to {outputCssPath}");
+                    CustomConsoleMessage(e?.Message ?? e?.ToString() ?? string.Empty);
+                }
             }
+        }
+
+        private static void CreateCssDocument(StringBuilder output, string spriteClassName, string outputFileName)
+        {
+            output.AppendLine("/* FEngSpriteGenerator: \u00a9 Futura Solutions GmbH. */");
+            output.AppendLine(string.Empty);
+            output.AppendLine($".{spriteClassName} {{");
+            output.AppendLine($"\tbackground-image:  url('{outputFileName}');");
+            output.AppendLine($"\tbackground-repeat: no-repeat;");
+            output.AppendLine($"\t/* Append here your custom styles... */");
+            output.AppendLine($"}}");
+        }
+
+        private static void AppendCss(StringBuilder output, string imageFileName, string cssPrefix, int x, int y)
+        {
+            var classImageFileName = Path.GetFileNameWithoutExtension(imageFileName).ToLower().Replace(" ", "");
+            output.AppendLine($"/* Original image file '{imageFileName}' */");
+            output.AppendLine($"{string.Format(cssPrefix, classImageFileName)} {{");
+            output.Append("\t");
+            output.Append($"background-position: {x * -1}px {y * -1}px;");
+            output.AppendLine("");
+            output.AppendLine($"}}");
+        }
+
+        private static bool HasArgument<T>(string argumentValue, T defaultValue, out T value)
+        {
+            value = defaultValue;
+            return argumentValue.TryParse(defaultValue, out value);
+        }
+    }
+    
+    static class GenericValueConverter
+    {
+        public static bool TryParse<T>(this string input, T defaultValue, out T result)
+        {
+            result = defaultValue;
+
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            if (converter != null)
+            {
+                try
+                {
+                    var convertedValue = (T)converter.ConvertFromString(input);
+                    if (convertedValue == null)
+                        return false;
+
+                    result = convertedValue;
+                    return true;
+                }
+                catch { }
+            }
+            return false;
         }
     }
 
--- readme.md
+++ readme.md
@@ -4,14 +4,36 @@
 
 Build project, open console and run application `FEngSpriteGenerator.exe` with next arguments:
 
-``` <input_directory> <output_directory> [align] [output_filename] [padding_value] ```
+``` --{ArgumentKey.InputDir}=<value> --{ArgumentKey.OutputDir}=<value> ```
+``` [--{ArgumentKey.Align}=<value>] [--{ArgumentKey.OutputFileName}=<value>] [--{ArgumentKey.PaddingValue}=<value>] ```
+``` [--{ArgumentKey.SpriteCssClassName}=<value>] [--{ArgumentKey.SpriteCssItemFormat}=<value>] ```
+``` [--{ArgumentKey.SpriteCssClassName}=<value>] [--{ArgumentKey.SpriteCssItemFormat}=<value>] ```
+``` [--{ArgumentKey.OutputCssFileName}=<value>] [--{ArgumentKey.OutputCssDir}=<value>] ```
 
 ### Argumenst:
 
 ```
-<input_directory>     Path to the directory containing input images.
-<output_directory>    Path to the directory where the generated sprite will be saved
--align                Optional. Alignment of the generated sprite (V for vertical, H for horizontal). Defaults to `Vertical`
--output_filename      Optional. Name of the generated sprite file. Defaults to `sprite_result.png`.
--padding_value        Optional. Padding between images in the sprite. Defaults to `10` pixels.
+--InputDir              - Path to the directory containing input images.");
+--OutputDir             - Path to the directory where the generated sprite will be saved.");
+--Align                 - Optional. Alignment of the generated sprite (V for vertical, H for horizontal). Defaults to 'Vertical'.");
+--OutputFileName        - Optional. Name of the generated sprite file. Defaults to 'sprite_result.png'.");
+--PaddingValue          - Optional. Padding between images in the sprite. Defaults to '10' pixels.");
+--SpriteCssClassName    - Optional. Name of base sprite css class. Defaults to 'sprite'.");
+--SpriteCssItemFormat   - Optional. Name of generated sprite class format. Defaults to '.sprite-icon-{0}'.");
+--OutputCssFileName     - Optional. Exported css file name. Defaults to '{defaultOutputCssFileName}'.");
+--OutputCssDir          - Optional. Path to the directory to export generated css. Defaults to output directory.");
 ```
+
+
+### How to use with project build?
+
+```
+...
+
+<Target Name="BeforeBuild" Condition="'$(Configuration)' == 'Debug'">
+    <Exec Command="$(SolutionDir)FEngSpriteGenerator.exe --InputDir=$(ProjectDir)Images\Img16 --OutputDir=$(ProjectDir)Images\Sprites --OutputFileName=nav16.png --SpriteCssClassName=sprites16 --OutputCssFileName=nav16sprites.css --OutputCssDir=$(ProjectDir)StyleSheets --SpriteCssItemFormat=&quot;.sprite-{0} .txt&quot;" />
+  </Target>
+...
+
+```
+This example demonstrates that we can utilize a command to invoke the generation of new sprites and styles for them, each time prior to project compilation. However, this applies only to the local version. Additionally, there is a hack to specify spaces for generating complex sprite classes.
